105faa1d3579d112a51f830c995c670ea7b57868
Morgan Bradley <mbradleystylist@gmail.com>
2017-12-07 01:07:24 +0000

    command line working! customer can select item id and quantity, js file checks stock levels

---
+++ bamazon.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(10) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tent", "Camping", 350.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sleeping Bag", "Camping", 150.00, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Backpack", "Camping", 200.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Water Filter", "Camping", 45.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pillow", "Camping", 40.00, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Water Bottle", "Camping", 15.00, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Camelbak", "Camping", 50.00, 45);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bug Spray", "Camping", 5.00, 80);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hiking Boots", "Camping", 150.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Percolator", "Camping", 30.00, 40);
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,107 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    showItemsAvailable();
+});
+
+function showItemsAvailable() {
+    var query = "SELECT item_id, product_name, department_name, price FROM products";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log("Products available for purchase: ");
+        console.log("");
+
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: $" + res[i].price);
+        }
+    });
+}
+
+
+// set timeout for itemANdQuantityMessage
+setTimeout(function() {
+    console.log("");
+    itemOrdered();
+}, 1000);
+
+
+function itemOrdered() {
+    inquirer.prompt([{
+        name: "item",
+        type: "input",
+        message: "What item would you like to purchase? Please enter Item ID."
+
+    }]).then(function(answer) {
+        var query = "SELECT * FROM products WHERE item_id=?";
+        connection.query(query, [answer.item], function(err, res) {
+            if (err) throw err;
+
+            for (var i = 0; i < res.length; i++) {
+                console.log("");
+                console.log(res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+                console.log("-----------------------------------");
+            }
+        });
+
+        quantityOrdered(answer.item);
+
+    });
+
+}
+
+function quantityOrdered(value) {
+    inquirer.prompt([{
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?"
+
+    }]).then(function(number) {
+        var query = "SELECT * FROM products WHERE item_id=?";
+        connection.query(query, [value], function(err, res) {
+            if (err) throw err;
+
+            for (var i = 0; i < res.length; i++) {
+                console.log("");
+                var stockLevel = res[i].stock_quantity;
+                var price = res[i].price;
+                console.log(res[i].product_name + " | " + stockLevel);
+                console.log("-----------------------------------");
+            }
+
+            if (stockLevel - number.quantity > 0) {
+                console.log("Thank you for choosing Bamazon! Your order has been placed.");
+                var totalCost = (number.quantity * price);
+                console.log("");
+                console.log("Total: $" + totalCost);
+            }
+
+            else if (stockLevel - number.quantity === 0) {
+                console.log("Just enough! Thank you for choosing Bamazon! Your order has been placed.");
+                console.log(stockLevel - number.quantity);
+
+            }
+
+            else if (stockLevel - number.quantity < 0) {
+                console.log("Insufficient quantity. Please try again.");
+
+            }
+
+            showItemsAvailable();
+        });
+    });
+}
